f20d70e6fb82c4f8325072e0cdd00ff9175e0cc3
gersoncardosofilho <gersoncfilho@mac.com>
2019-04-21 04:03:22 -0300

    Implementação do Swagger

---
+++ InfoTrader.Domain/Interfaces/IContactRepository.cs
@@ -0,0 +1,17 @@
+﻿using InfoTrader.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoTrader.Domain.Interfaces
+{
+    public interface IContactRepository
+    {
+        Task<IEnumerable<Contact>> GetAllAsync();
+        Task Create(Contact contact);
+        Task<Contact> GetContactById(int contactId);
+        Task<bool> Delete(int contactId);
+        Task<bool> Update(Contact newCustomer);
+    }
+}
--- InfoTrader.Web/Startup.cs
+++ InfoTrader.Web/Startup.cs
@@ -16,6 +16,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace InfoTrader.Web
 {
@@ -38,7 +39,13 @@ namespace InfoTrader.Web
                     fv.ImplicitlyValidateChildProperties = true;
                 });
             services.AddResponseCompression();
-            
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
+
             services.AddTransient<ICustomerRepository, CustomerRepository>();
             services.AddTransient<ICustomerContext, CustomerContext>();
             services.AddTransient<IValidator<Customer>, CustomerValidator>();
@@ -55,6 +62,21 @@ namespace InfoTrader.Web
             {
                 app.UseHsts();
             }
+
+            app.UseStaticFiles();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseMvc();
+
 
             app.UseHttpsRedirection();
             app.UseMvc();
